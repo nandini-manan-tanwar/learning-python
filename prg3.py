f086d2c4caa887af665afd55aa5d4299964b911d
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-06-04 14:02:28 +0530

    update prg3.py

--- prg3.py
+++ prg3.py
@@ -1,12 +1,14 @@
-name="nandu"
+name="" 
 age=18
 cgpa=8.61
 is_student=False
 cgpa=int(cgpa)
 age=str(age)
 age+='of nandini'
+name=bool(name)
 
 print(f"cgpa={cgpa}")
 print(f"age={age}")
 print(type(age))
+print(f"name={name}")
 
