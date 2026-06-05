e26f48a56b446624ecdd9c6596062b2ad84ede9b
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-06-05 11:43:31 +0530

    added prg5.py

---
+++ prg5.py
@@ -0,0 +1,15 @@
+# area of rectangle 
+length=int(input("enter the length of the rectangle"))
+breadth=int(input("enter breadth of rectangle"))
+area_of_rectangle= length*breadth
+print(f"area={area_of_rectangle}cm²")
+
+# shopping cart 
+item=input("enter the item you want")
+price=int(input("what is the price of the item"))
+quantity=int(input("number of items"))
+total_price=price*quantity
+print(f"the total price for your purchase of ")
+print(f"item:{item}")
+print(f"quantity:{quantity} is")
+print (f"price ={total_price}")
