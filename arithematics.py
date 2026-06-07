c1cebf275df0469c770ce33d913d7edb0a5eeb1d
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-06-07 14:52:45 +0530

    Added prg4.py

---
+++ arithematics.py
@@ -0,0 +1,9 @@
+# circumference of a circle 
+import math
+radius=float(input("enter radius of circle"))
+circumference = 2*math.pi*radius
+print(round(circumference))
+
+#area of circle
+area=math.pi*pow(radius,2)
+print(round(area))
