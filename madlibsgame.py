a9de9a19866c0086561350e76b183d8d370b790b
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-06-07 11:19:57 +0530

    update madlibsgame.py

---
+++ madlibsgame.py
@@ -0,0 +1,15 @@
+# fill random words ,the sentences are unkown 
+noun1=input("enter the noun {a person,place etc}")
+adjective1=input("enter an adjective {Beautiful,Tall}")
+Verb1=input("enter a verb")
+noun2=input("enter noun2")
+adjective2=input("enter adjective2")
+verb2=input("enter verb2")
+verb3=input("enter verb3")
+
+
+print(f"her name is {noun1},they sound {adjective1} and loves to plays {Verb1}")
+print(f"so i saw a {noun2} ")
+print(f"it was {adjective2}")
+print(f"it was doing{verb2}")
+print(f"i was {verb3}")
