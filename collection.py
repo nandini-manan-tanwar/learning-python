aa2e129e794cd74cb2562e0f19e88f931560ba53
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-06-14 13:58:25 +0530

    add collection

---
+++ collection.py
@@ -0,0 +1,35 @@
+# # collection = single "variable" used to store multiple values
+# # List  = [] ordered and changeable. Duplicates OK
+# # Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
+# # Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+
+fruits={'apple','banana','orange','mango'}
+fruits.pop()
+print(fruits)
+# print(fruits)
+
+# for fruit in fruits:
+#     print(fruit)
+
+# print('\n')
+# print(fruits[2])
+# print('\n')
+# print(len(fruits))
+# print('\n')
+# print("apple" in fruits)
+# print('\n')
+# fruits[0]='pineapple' 
+# print('\n')
+# for fruit in fruits:
+#     print(fruit)
+# print('\n')
+# fruits.append("papaya")  
+# print(fruits)
+# print('\n')
+# fruits.remove('mango')
+# print(fruits)
+# fruits.insert(4,'ily')
+# print('\n')
+# print(fruits)
+# fruits.sort()
+# print(fruits)
