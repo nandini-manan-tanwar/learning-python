36d69bb8cab2bbea9ebaea7e6a4ee9e0cb0f1c9c
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-07-09 23:14:29 +0530

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+#if_name == _main: (this script can be imported OR run standalone)
+
+# Functions and classes in this module can be reused # without the main block of code executing
+# ex. library Import library for functionality
+
+def main():
+  #Your program goes here
+  print("Running main function")
+  
+
+
+if __name__ == "__main__":
+    main()
+#now we gonna test it in new file in test_import.py
