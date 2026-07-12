206a00ca05e69c52e0e066fccb47d177bccef8a3
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-07-12 18:41:31 +0530

    update slotmachine

--- slotmachine.py
+++ slotmachine.py
@@ -5,6 +5,28 @@ def spin_row ():
 
      return[random.choice(symbols)for _ in range(3)]
 
+def print_row(row):
+     print("*******************")
+     print("-".join(row))
+     print("*******************")
+
+def get_payout(row,bet):
+   if row[0]==row[1]==row[2]:
+       if row[0]=="🍒":
+          return int(bet)*2   
+       elif row[0]=="🌸":
+          return int(bet)*3
+       elif row[0]=="💛":
+          return int(bet)*4     
+       else :
+          return int(bet)*5     
+    
+   else:
+       return 0
+
+  
+
+
 
 print("*************************")
 print("welcome to slotty machine")
@@ -26,7 +48,21 @@ while balance>0:
          
     balance=balance-int(bet)
     row=spin_row()
-    print(row) 
-         
+    print("spinning......")
+    print_row(row) 
+
+    payout=get_payout(row,bet)
+    if int(payout)>0:
+        print(f"you won ${payout}")
+    else:
+        print("sorry you lost this round")
           
-    
+    balance+=int(payout)
+    answer=input(("do you wanna play again?(Y/N)"))
+    if answer.upper()=="N":
+        break
+    elif answer.upper()=="Y":
+        continue
+    else:
+        answer=input("enter only y or n")
+
